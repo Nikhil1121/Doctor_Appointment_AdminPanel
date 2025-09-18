02b9b6fdd0d367794fcef228c33ea714c2a83756
Nikhil shendre <nikhilshendre008@gmail.com>
2025-09-18 18:12:50 +0530

    applying on change property on it

--- src/pages/Admin/AddDoctor.jsx
+++ src/pages/Admin/AddDoctor.jsx
@@ -23,9 +23,9 @@ const AddDoctor = () => {
       <div className="bg-white px-8 py-6 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll">
         <div className="flex items-center gap-4 mb-8 text-gray-500">
           <label htmlFor="doc-img">
-            <img className="w-16 bg-gray-100 rounded-full cursor-pointer" src={assets.upload_area} alt="" />
+            <img className="w-16 bg-gray-100 rounded-full cursor-pointer" src={docImg ? URL.createObjectURL(docImg) : assets.upload_area} alt="" />
           </label>
-          <input type="file" id="doc-img" hidden />
+          <input onChange={(e)=> setDocImg(e.target.files[0])} type="file" id="doc-img" hidden />
           <p>
             Upload doctor <br /> picture
           </p>
